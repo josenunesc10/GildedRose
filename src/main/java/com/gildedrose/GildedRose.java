72c472b2a81637657d4556f7985ba072bae2f840
RK5585 <jose.nunes@kony.com>
2019-09-06 16:13:01 +0100

    Isolated Sulfuras and Backstage pass  items update logic

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -16,7 +16,11 @@ class GildedRose {
 	private void updateItem(Item item) {
 		if (item.name.startsWith("Aged Brie")) {
 			updateAgedBrieItem(item);
-		} else {
+		} else if (item.name.startsWith("Sulfuras")) {
+			updateSulfurasItem(item);
+		} else if (item.name.startsWith("Backstage pass")) {
+			updateBackstagePassItem(item);
+		}else{
 			if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 				if (item.quality > 0) {
 					if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -77,5 +81,32 @@ class GildedRose {
 				item.quality = item.quality + 1;
 			}
 		}
+	}
+	
+	private void updateSulfurasItem(Item item) {
+		// Do nothing!
+	}
+	
+	private void updateBackstagePassItem(Item item) {
+		if (item.quality < 50) {
+			item.quality = item.quality + 1;
+			
+			if (item.sellIn < 11) {
+				if (item.quality < 50) {
+					item.quality = item.quality + 1;
+				}
+			}
+
+			if (item.sellIn < 6) {
+				if (item.quality < 50) {
+					item.quality = item.quality + 1;
+				}
+			}
+		}
+		item.sellIn = item.sellIn - 1;
+		if (item.sellIn < 0) {
+			item.quality = item.quality - item.quality;
+		}
+		
 	}
 }
