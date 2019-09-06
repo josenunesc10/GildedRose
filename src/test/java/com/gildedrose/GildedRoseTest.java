e4923201917013f98a170db09bc0dbaf6c552285
RK5585 <jose.nunes@kony.com>
2019-09-06 15:52:58 +0100

    Added unit tests for Backstage pass item

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -13,9 +13,13 @@ public class GildedRoseTest {
 	private Item createAgedBrieItem(int sellIn, int quality) {
 		return new Item("Aged Brie", sellIn, quality);
 	}
-	
+
 	private Item createSulfurasItem(int sellIn, int quality) {
 		return new Item("Sulfuras, Hand of Ragnaros", sellIn, quality);
+	}
+
+	private Item createBackstagePassItem(int sellIn, int quality) {
+		return new Item("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
 	}
 
 	@Test
@@ -129,24 +133,133 @@ public class GildedRoseTest {
 		// The Quality of an item is never more than 50
 		assertEquals(50, app.items[0].quality);
 	}
-	
+
 	@Test
 	public void testSulfurasNoDegradationOnSellIn() {
 		Item[] items = new Item[] { createSulfurasItem(0, 80) };
 		GildedRose app = new GildedRose(items);
 		app.updateQuality();
-		// "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+		// "Sulfuras", being a legendary item, never has to be sold or decreases in
+		// Quality
 		assertEquals(0, app.items[0].sellIn);
 		assertEquals(80, app.items[0].quality);
 	}
-	
+
 	@Test
 	public void testSulfurasNoDegradationOrSellInChange() {
 		Item[] items = new Item[] { createSulfurasItem(10, 50) };
 		GildedRose app = new GildedRose(items);
 		app.updateQuality();
-		// "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+		// "Sulfuras", being a legendary item, never has to be sold or decreases in
+		// Quality
 		assertEquals(10, app.items[0].sellIn);
+		assertEquals(50, app.items[0].quality);
+	}
+
+	@Test
+	public void testBackstagePassMoreThan10DaysSellInDegradation() {
+		Item[] items = new Item[] { createBackstagePassItem(20, 20) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		// "Backstage passes", like aged brie, increases in Quality as its SellIn value
+		// approaches;
+		assertEquals(19, app.items[0].sellIn);
+		assertEquals(21, app.items[0].quality);
+	}
+
+	@Test
+	public void testBackstagePassOn11DaysSellInDegradation() {
+		Item[] items = new Item[] { createBackstagePassItem(11, 20) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		// "Backstage passes", like aged brie, increases in Quality as its SellIn value
+		// approaches;
+		assertEquals(10, app.items[0].sellIn);
+		assertEquals(21, app.items[0].quality);
+	}
+
+	@Test
+	public void testBackstagePassOn10DaySellInDegradation() {
+		Item[] items = new Item[] { createBackstagePassItem(10, 20) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals(9, app.items[0].sellIn);
+		// Quality increases by 2 when there are 10 days or less and by 3 when there are
+		// 5 days or less
+		assertEquals(22, app.items[0].quality);
+	}
+
+	@Test
+	public void testBackstagePassOn6DaysSellInDegradation() {
+		Item[] items = new Item[] { createBackstagePassItem(6, 20) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals(5, app.items[0].sellIn);
+		// Quality increases by 2 when there are 10 days or less and by 3 when there are
+		// 5 days or less
+		assertEquals(22, app.items[0].quality);
+	}
+
+	@Test
+	public void testBackstagePassOn5DaysSellInDegradation() {
+		Item[] items = new Item[] { createBackstagePassItem(5, 20) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals(4, app.items[0].sellIn);
+		// Quality increases by 2 when there are 10 days or less and by 3 when there are
+		// 5 days or less
+		assertEquals(23, app.items[0].quality);
+	}
+
+	@Test
+	public void testBackstagePassOn1DaySellInDegradation() {
+		Item[] items = new Item[] { createBackstagePassItem(1, 20) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals(0, app.items[0].sellIn);
+		// Quality increases by 2 when there are 10 days or less and by 3 when there are
+		// 5 days or less
+		assertEquals(23, app.items[0].quality);
+	}
+
+	@Test
+	public void testBackstagePassOnSellInDegradation() {
+		Item[] items = new Item[] { createBackstagePassItem(0, 20) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals(-1, app.items[0].sellIn);
+		// Quality drops to 0 after the concert
+		assertEquals(0, app.items[0].quality);
+	}
+
+	@Test
+	public void testBackstagePassAfterSellInDegradation() {
+		Item[] items = new Item[] { createBackstagePassItem(-10, 0) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals(-11, app.items[0].sellIn);
+		// Quality drops to 0 after the concert
+		assertEquals(0, app.items[0].quality);
+	}
+
+	
+	@Test
+	public void testBackstagePassLimitDegradation() {
+		Item[] items = new Item[] { createBackstagePassItem(5, 49) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals(4, app.items[0].sellIn);
+		// an item can never have its Quality increase above 50
+		assertEquals(50, app.items[0].quality);
+	}
+	
+	@Test
+	public void testBackstagePassAtMaxQualityLimitDegradation() {
+		Item[] items = new Item[] { createBackstagePassItem(5, 50) };
+		GildedRose app = new GildedRose(items);
+		app.updateQuality();
+		assertEquals(4, app.items[0].sellIn);
+		// an item can never have its Quality increase above 50
 		assertEquals(50, app.items[0].quality);
 	}
 }
