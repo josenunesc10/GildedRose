244f159ee2a9810d7daa6d4e35c4cb7bfec10a7a
RK5585 <jose.nunes@kony.com>
2019-09-06 16:16:16 +0100

    Isolated generic item update logic

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -20,54 +20,8 @@ class GildedRose {
 			updateSulfurasItem(item);
 		} else if (item.name.startsWith("Backstage pass")) {
 			updateBackstagePassItem(item);
-		}else{
-			if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-				if (item.quality > 0) {
-					if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-						item.quality = item.quality - 1;
-					}
-				}
-			} else {
-				if (item.quality < 50) {
-					item.quality = item.quality + 1;
-
-					if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-						if (item.sellIn < 11) {
-							if (item.quality < 50) {
-								item.quality = item.quality + 1;
-							}
-						}
-
-						if (item.sellIn < 6) {
-							if (item.quality < 50) {
-								item.quality = item.quality + 1;
-							}
-						}
-					}
-				}
-			}
-
-			if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-				item.sellIn = item.sellIn - 1;
-			}
-
-			if (item.sellIn < 0) {
-				if (!item.name.equals("Aged Brie")) {
-					if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-						if (item.quality > 0) {
-							if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-								item.quality = item.quality - 1;
-							}
-						}
-					} else {
-						item.quality = item.quality - item.quality;
-					}
-				} else {
-					if (item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
-				}
-			}
+		} else {
+			updateGenericItem(item);
 		}
 	}
 
@@ -82,15 +36,15 @@ class GildedRose {
 			}
 		}
 	}
-	
+
 	private void updateSulfurasItem(Item item) {
 		// Do nothing!
 	}
-	
+
 	private void updateBackstagePassItem(Item item) {
 		if (item.quality < 50) {
 			item.quality = item.quality + 1;
-			
+
 			if (item.sellIn < 11) {
 				if (item.quality < 50) {
 					item.quality = item.quality + 1;
@@ -107,6 +61,19 @@ class GildedRose {
 		if (item.sellIn < 0) {
 			item.quality = item.quality - item.quality;
 		}
-		
+
+	}
+
+	private void updateGenericItem(Item item) {
+		if (item.quality > 0) {
+			item.quality = item.quality - 1;
+		}
+		item.sellIn = item.sellIn - 1;
+
+		if (item.sellIn < 0) {
+			if (item.quality > 0) {
+				item.quality = item.quality - 1;
+			}
+		}
 	}
 }
